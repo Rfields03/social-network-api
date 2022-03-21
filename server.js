e33be0047c404273c024345c517b474f36539fd8
Robert Field <rfields03@gmail.com>
2022-03-21 18:12:53 -0400

    update json

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.use(require('./routes'));
@@ -19,4 +20,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
