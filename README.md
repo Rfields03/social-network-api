fb462c5a7556df5b8cab1293cd3d13b43161c139
Robert Field <rfields03@gmail.com>
2022-03-22 15:49:37 -0400

    improve code in server

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ AS A social media startup
 I WANT an API for my social network that uses a NoSQL database
 SO THAT my website can handle large amounts of unstructured data
 
+
+## Description 
+This application is for creating an API for a social media network that can handle large amounts of unstructured data utilizing a NoSQL database. The user has the ability to add, delete, get, and update users, friends of users, thoughts, and reactions on those thoughts.
+
+## Installation
+Clone the application from the command line and run 'npm i' to install the necessary dependencies, then run the command "npm start" from the command line. 
+
+## Usage
+To run this application, run: npm start to use on a local server 3001 after cloning the repository and installing the necessary dependencies.
+
+### Built With 
+
+ * JavaScript
+ * Node JS
+ * NoSQL - MongoDB
+ * Mongoose
+ * Moment
+ * Express
+
+## Demonstrations
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+const app = express();
+
 app.use(express.urlencoded({ extended: true }
 ));
+app.use(express.json());
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/social-network-api";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+// routes
+app.use(require("./routes/api"));
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Connected on localhost:${PORT}!`);
+});
