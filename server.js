ee5de810bf6f5c9d68781eaff8080c0e7ec6d44b
Robert Field <rfields03@gmail.com>
2022-03-22 15:35:34 -0400

    establish 3001 connection

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }
+));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
